eabcb3c19d9e0ff38fcbe3189d4c9add92b97190
mofeimw <mofei.mw@gmail.com>
2024-07-23 00:48:49 -0500

    Initial Commit

---
+++ Frames/FramesApp.swift
@@ -0,0 +1,20 @@
+//
+//  FramesApp.swift
+//  Frames
+//
+//  Created by mofei wang on 7/23/24.
+//
+
+import SwiftUI
+
+@main
+struct FramesApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
